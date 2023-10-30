9935389a1f530770fb51accfa8328c931850590c
Lordhermano <146134282+Lordhermano@users.noreply.github.com>
2023-10-30 22:23:55 +0000

    Correction test_bubble_sort.py
    The Methods were not indented properly so here is a corrected version

--- tests/test_bubble_sort.py
+++ tests/test_bubble_sort.py
@@ -6,13 +6,13 @@ class Test_bubble_sort(unittest.TestCase):
    def test_float(self):
         result=MathTL.bubble_sort(9.0,5.5,3.3,6,0)
         self.assertEqual(result,[0,3.3,5.5,6,9.0])
-    def test_neg(self):
+   def test_neg(self):
         result=MathTL.bubble_sort(-2,4,6,0)
         self.assertEqual(result,[-2,0,4,6])
-    def test_dups(self):
+   def test_dups(self):
         result=MathTL.bubble_sort(2,2,2,5,7,1,-9)
         self.assertEqual(result,[-9,1,2,2,2,5,7])
-    def test_string(self):
+   def test_string(self):
         result=MathTL.bubble_sort('d','f','g','a','b' )
         self.assertEqual(result,['a','b','d','f','g'])
 if __name__='main':
